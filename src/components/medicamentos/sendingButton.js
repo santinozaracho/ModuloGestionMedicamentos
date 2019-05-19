26640a1570c05126e77b83c9f38fea1d43997527
Carlos Santino Zaracho Simonetto <40505201+santinozaracho@users.noreply.github.com>
2019-05-18 23:10:20 -0300

    Incorporacion de los Errores enn los botones

--- src/components/medicamentos/sendingButton.js
+++ src/components/medicamentos/sendingButton.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import {Button,InputGroup,FormControl} from 'react-bootstrap';
 
-// function simulateNetworkRequest() {
-//     return new Promise(resolve => setTimeout(resolve, 2000));
-//   }
   class SendingButton extends React.Component {
     constructor(props, context) {
       super(props, context);
@@ -11,6 +8,7 @@ import {Button,InputGroup,FormControl} from 'react-bootstrap';
       this.handleClick = this.handleClick.bind(this);
       this.handleInputChange = this.handleInputChange.bind(this);
       this.state = {
+        loadingText:"Espere...",
         isLoading: false,
         isCommon:true,
         url:'https://us-central1-modulogestionmedicamentos.cloudfunctions.net/app/medicamentos',
@@ -43,16 +41,17 @@ import {Button,InputGroup,FormControl} from 'react-bootstrap';
       this.setState({ isLoading: false });
     }
 
-    async updateMedicament(url,method,sendObj) {
-      console.log(sendObj);
-      const response = await fetch(url, { method:method, 
+    async updateMedicament() {
+      let sendObj = {};
+      sendObj.refId = this.props.refId
+      sendObj.cantidad = this.state.cantidad;
+      const response = await fetch(this.state.url, { method:this.state.method, 
                   redirect: 'follow',
                   headers: {
                             'Accept': 'application/json',
                             'Content-Type': 'application/json'
                           },
-                    body: JSON.stringify(sendObj)
-                  });
+                    body: JSON.stringify(sendObj)});
       return response
     }
 
@@ -65,53 +64,52 @@ import {Button,InputGroup,FormControl} from 'react-bootstrap';
   }
 
     async handleClick(e) {
-      let sendObj = this.state;
       this.setState({ isLoading: true }); 
-      sendObj.docRef = this.props.docRef;
-      const resp = await this.updateMedicament(this.state.url,this.state.method,sendObj);
+      const resp = await this.updateMedicament();
       if (resp.ok) {
-        console.log(resp);
         this.setState({cantidad:'',isLoading:false});
         this.props.onListenEv(e);
+      }else{
+        this.setState({loadingText:"ERROR:"+resp.status})
+      
       }
     }
   
     render() {
-      
       const { isLoading } = this.state;
       const { isCommon } = this.state;
 
       return (
-        <div>
-        {isCommon ? 
+        <>
+        {isCommon ?( 
           <InputGroup className="mb-3">
-            {isLoading ? '' : 
-            <FormControl width="25" value={this.state.cantidad}
-              onChange = {this.handleInputChange}
-                name="cantidad"
-                placeholder="Cantidad"
-                aria-label="Recipient's new Stock"
-                aria-describedby="basic-addon"/> 
-            }
-            <InputGroup.Append>
-                <Button variant={isLoading ? "secondary" : this.state.color}
-                block
-                disabled={isLoading}
-                onClick={!isLoading ? this.handleClick : null}>
-                {isLoading ? 'Enviando...' : this.state.button}
-                </Button>
-            </InputGroup.Append>
-        </InputGroup>  
+            {!isLoading && (<FormControl required 
+                                className="w-25" 
+                                value={this.state.cantidad}
+                                onChange = {this.handleInputChange}
+                                name="cantidad"
+                                placeholder="Cantidad"
+                                aria-label="Recipient's new Stock"
+                                aria-describedby="basic-addon"/>
+                            )}
+            <Button className={isLoading ? "w-100" : "w-75"}
+            variant={isLoading ? "secondary" : this.state.color}   
+            disabled={isLoading}
+            onClick={!isLoading ? this.handleClick : null}>
+            {isLoading ? this.state.loadingText : this.state.button}
+            </Button>
+            
+          </InputGroup>)
         : 
-        <InputGroup className="mb-3">
-          <Button variant={isLoading ? "secondary" : this.state.color}
-          block
-          disabled={isLoading}
-          onClick={!isLoading ? this.handleClick : null}>
-          {isLoading ? 'Enviando...' : this.state.button}
-          </Button>
-        </InputGroup>}
-        </div>
+          (<InputGroup className="mb-3">
+            <Button variant={isLoading ? "secondary" : this.state.color}
+            block
+            disabled={isLoading}
+            onClick={!isLoading ? this.handleClick : null}>
+            {isLoading ? this.state.loadingText : this.state.button}
+            </Button>
+          </InputGroup>)}
+        </>
       );
     }
   }
